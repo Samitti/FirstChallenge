2665e0bf72c02a69efc5d8efb167229b18979b81
Samuel <samuelteweldebrhan@gmail.com>
2025-01-14 21:00:12 -0500

    added the correct code

--- FirstChallenge/Program.cs
+++ FirstChallenge/Program.cs
@@ -8,8 +8,19 @@ namespace FirstChallenge
 {
     internal class Program
     {
+        public static Func<int> GetRandomNumber = () => new Random().Next(1, 10000);
+
+        public static Func<int, string> GenerateMessage = (number) => $"the random numer is: {number}";
         static void Main(string[] args)
         {
+            //Console.WriteLine(GetRndomNumer());
+            //Console.WriteLine(GenerateMessage(2));
+
+            var task = Task.Factory.StartNew(() => GetRandomNumber())  
+                .ContinueWith(t => GenerateMessage(t.Result))  
+                .ContinueWith(t => Console.WriteLine(t.Result));
+
+            task.Wait();
         }
     }
 }
